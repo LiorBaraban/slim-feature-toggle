f737937191031c6015969a123dd753deb2f68f42
Lior Baraban <liorb@taldor.co.il>
2021-05-03 11:09:16 +0300

    lib - added version and feature toggle

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports.featureToggle = require('./lib/feature-toggle');
+module.exports.versionToggle = require('./lib/version-toggle');
+module.exports.APP_VERSION_IS = require('./lib/app-version-is');
