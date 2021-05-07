3474090f411e423a04870f7e8503c229498b144a
Lior Baraban <oracleilb@gmail.com>
2021-05-07 20:31:02 +0300

    playground

--- playground.js
+++ playground.js
@@ -1,3 +1,18 @@
-const { getPackageJsonVersion} = require('./lib/version-toggle');
+const { APP_VERSION_IS } = require('./lib/app-version-is')
 
-console.log(getPackageJsonVersion());
+const { versionToggleRunPromiseWhen, versionToggleRunCallbackWhen } = require('./lib/version-toggle');
+
+
+
+versionToggleRunPromiseWhen(APP_VERSION_IS.GREATER_THAN, '1.0.0')
+  .then(() => {
+    console.log('promise yay');
+  })
+  .catch(() => {
+    console.log('promise nay');
+  })
+
+
+versionToggleRunCallbackWhen(APP_VERSION_IS.EQUAL_TO, '1.0.0', () => {
+  console.log('callback yay');
+})
